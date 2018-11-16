ad68f975fcf27022d3ebb6bff48ecd03fe09fbe9
Murat Mavlidov <mmavlidov96@mail.ru>
2018-11-16 08:50:47 +0300

    run build

---
+++ public/precache-manifest.ccebbde5cf1d146b54113bf79388a532.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "04187d0e585e0ec6b355",
+    "url": "/static/js/main.04187d0e.chunk.js"
+  },
+  {
+    "revision": "0546294e2a6c2ef5ae9e",
+    "url": "/static/js/1.0546294e.chunk.js"
+  },
+  {
+    "revision": "04187d0e585e0ec6b355",
+    "url": "/static/css/main.9557f069.chunk.css"
+  },
+  {
+    "revision": "8a4aa3d5eeb2910d42bb1e71d74dcf4b",
+    "url": "/index.html"
+  }
+];
